6707c1c15e840cef125ee49d8a51ae8cda47aa3f
Evgeny Levinsky <levinsky@aheadworks.com>
2024-03-04 16:00:34 -0600

    Changed image values in the product eav table after gallery

--- Model/ResourceModel/GalleryImage.php
+++ Model/ResourceModel/GalleryImage.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Elevinskii\MediaStorage\Model\ResourceModel;
 
 use Magento\Catalog\Model\ResourceModel\Product\Gallery;
+use Magento\Framework\Model\AbstractModel;
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
 
 class GalleryImage extends AbstractDb
@@ -16,5 +17,27 @@ class GalleryImage extends AbstractDb
     protected function _construct()
     {
         $this->_init(Gallery::GALLERY_TABLE, 'value_id');
+    }
+
+    /**
+     * Change in EAV product table after gallery save
+     *
+     * @param AbstractModel $object
+     * @return $this
+     */
+    protected function _afterSave(AbstractModel $object)
+    {
+        $imagePath = $object->getData('value');
+        $originImagePath = $object->getOrigData('value');
+
+        if ($imagePath !== $originImagePath) {
+            $this->getConnection()->update(
+                $this->getTable('catalog_product_entity_varchar'),
+                ['value' => $imagePath],
+                ['value = ?' => $originImagePath]
+            );
+        }
+
+        return parent::_afterSave($object);
     }
 }
